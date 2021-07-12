896c2e27ab9f8a2b451282f549a951167b493522
Alon <Alonsouter@gmail.com>
2021-07-12 21:40:39 +0300

    Order and fixes
    Scripts, prefabs, animation files, similar assets in different places, old/deprecated folder - much improve, such order, so wow.

---
+++ Assets/Scripts/Base/Squad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squad 
+{
+    //data group of which pawns make this group up
+
+    //contains modified-pawns (as refs to prefabs?)
+    //could instead have list of names and a DIFFERENT list for modifiers (if any)
+
+
+}
---
+++ Assets/Scripts/Save System/SaveData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData 
+{
+    //availavle mercs
+
+    //squads?
+
+    //gold
+    //site cooldowns?
+    //
+}
