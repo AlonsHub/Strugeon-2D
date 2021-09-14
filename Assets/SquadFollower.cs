a615e0e94f279a247d144e9b68fa966a60e28a92
Alon <Alonsouter@gmail.com>
2021-09-14 19:20:02 +0300

    Squad Picker local logic DONE
    - Squad picker holds the INDEX for which squad is currently active/chosen
    - Frame sprites swap to "SelectedFrame" and "BaseFrame" when selected and deselected (respectively).
    - Follower is more adherent to time, but there is a disconnect there between the stated duration. 
    - Speed is calculated by dividing distance by TotalDuration, and distance traveled is added by speed*Time.deltaTime so it MUST take 1 second to move "speed" distance. The math checks out, reality however - seems to disagree

--- Assets/SquadFollower.cs
+++ Assets/SquadFollower.cs
@@ -19,7 +19,6 @@ public class SquadFollower : MonoBehaviour
     private void Start()
     {
         pathFollower = GetComponent<PathFollower>();
-      
     }
 
     public void SetMe(Squad s, float t, PathCreator p)
@@ -31,19 +30,17 @@ public class SquadFollower : MonoBehaviour
         pathFollower.speed = p.path.cumulativeLengthAtEachVertex[p.path.cumulativeLengthAtEachVertex.Length - 1]/t;
         pathFollower.pathCreator = path;
 
-        //pathFollower.speed = 
     }
 
     public void Go()
     {
-        //pathFollower.
+
     }
 
     IEnumerator RunTimer()
     {
         while (remainingTime >= 0)
         {
-            //TimeSpan ts = new TimeSpan(0, (int)Math.Floor(remainingTime/60), (int)remainingTime);
             TimeSpan ts = new TimeSpan(0, (int)remainingTime/60, (int)remainingTime);
             timeText.text = string.Format("{0:D2}:{1:D2}", ts.Minutes, ts.Seconds);
             yield return new WaitForSeconds(timerRate);
---
+++ Assets/SquadToggler.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SquadToggler : MonoBehaviour
+{
+    //public value of chosen squad
+    public int selectedToggle = -1; //defualt/no-option-picked = -1
+
+    //all my toggles
+    Toggle[] myToggles;
+
+    GameObject[][] portraits = new GameObject[4][]; //this 4 is dependant on number of squads
+    
+    [SerializeField]
+    GameObject[] emptyRowParents;
+    [SerializeField]
+    Sprite offFrame;
+    [SerializeField]
+    Sprite onFrame;
+
+    //on value changed
+
+    private void Start()
+    {
+        myToggles = GetComponentsInChildren<Toggle>();
+
+        if (myToggles == null)
+        {
+            Debug.LogError("No toggles babbbbyyyyyy");
+            return;
+        }
+
+        //check available squads
+
+        for (int i = 0; i < myToggles.Length; i++)
+        {
+            if (i < PartyMaster.Instance.squads.Count)
+            {
+                myToggles[i].onValueChanged.AddListener(delegate { ValuesChanged(); });
+            }
+            else
+            {
+                myToggles[i].gameObject.SetActive(false);
+            }
+        }
+
+        //foreach (var t in myToggles)
+        //{
+        //    t.onValueChanged.AddListener(delegate { ValuesChanged(); });
+        //}
+    }
+
+    void ValuesChanged()
+    {
+        if (selectedToggle != -1)
+        {
+            for (int i = 0; i < PartyMaster.Instance.squads[selectedToggle].pawns.Count; i++)
+            {
+                emptyRowParents[selectedToggle].transform.GetChild(i).GetComponent<Image>().sprite = offFrame;
+            }
+        }
+
+        for (int i = 0; i < myToggles.Length; i++)
+        {
+            if(myToggles[i].isOn)
+            {
+                selectedToggle = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < PartyMaster.Instance.squads[selectedToggle].pawns.Count; i++)
+        {
+            emptyRowParents[selectedToggle].transform.GetChild(i).GetComponent<Image>().sprite = onFrame;
+        }
+    }
+}
