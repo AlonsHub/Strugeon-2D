f59b4c989e5f4915d6537ae62559a00fc1fc0f56
Alon <Alonsouter@gmail.com>
2021-10-14 20:00:24 +0300

    SquadRoom Started

---
+++ Assets/Scripts/UI Scripts/BasicMercSlot.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BasicMercSlot : MonoBehaviour, IPointerClickHandler
+{
+    public Pawn merc;
+
+    [SerializeField]
+    Image portait;
+    [SerializeField]
+    Image bgFrame;
+
+    //should NOT be in the basic form
+    [SerializeField]
+    SquadRoomDisplayer squadRoomDisplayer;
+    [SerializeField]
+    Sprite onSprite;
+    [SerializeField]
+    Sprite offSprite;
+    [SerializeField]
+    bool isOccupied;
+
+
+    public void SetMe(Pawn p)
+    {
+        merc = p;
+        portait.sprite = merc.PortraitSprite;
+        portait.color = new Color(1, 1, 1, 1);
+        isOccupied = true;
+    }
+    public void SetMe()
+    {
+        portait.color = new Color(0, 0, 0, 0);
+        isOccupied = false;
+    }
+
+    public void FrameToggle(bool turnOn)
+    {
+        bgFrame.sprite = turnOn ? onSprite : offSprite;
+        //if (turnOn)
+        //{
+        //    bgFrame.sprite = onSprite;
+        //}
+        //else
+        //{
+        //    bgFrame.sprite = offSprite;
+        //}
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+
+            squadRoomDisplayer.ClickedMercSlot(this);
+            //bg_img.sprite = onBgSprite;
+        }
+    }
+}
---
+++ Assets/SquadRoomDisplayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SquadRoomDisplayer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject squadSlotPrefab;
+    [SerializeField]
+    TMP_Text infoText;
+    [SerializeField]
+    Image roomImage;
+    [SerializeField]
+    MercDataDisplayer mercDataDisplayer;
+
+    BasicMercSlot[] squadSlots;
+    public void SetMe(Room room)
+    {
+        squadSlots = new BasicMercSlot[room.size];
+        for (int i = 0; i < squadSlots.Length; i++)
+        {
+            //squadSlots[i]
+        }
+    }
+
+    public void ClickedMercSlot(BasicMercSlot mercSlot)
+    {
+        foreach (var slot in squadSlots)
+        {
+            slot.FrameToggle(slot.Equals(mercSlot));
+        }
+        SetMercDataDisplayer(mercSlot.merc);
+    }
+    void SetMercDataDisplayer(Pawn merc)
+    {
+        if (!mercDataDisplayer.gameObject.activeSelf)
+            mercDataDisplayer.gameObject.SetActive(true);
+
+        mercDataDisplayer.SetMe(merc);
+    }
+}
