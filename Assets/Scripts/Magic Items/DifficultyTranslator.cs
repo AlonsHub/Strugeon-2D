1f9805e7a76e109a1ef518f4fdbc3c3e24dde4e5
Alon <Alonsouter@gmail.com>
2022-02-17 17:31:38 +0200

    ItemSets and EnemySets DONE
    Difficulty translator - is a script responsible for setting level data to sites (sites call it to ask for new data when the bool isSet == false.
    
    difficulty translator now has:
    easy/medium/hard enemy sets
    easy/medium/hard item sets
    
    when a site is set (by difficulty) - it randomizes which enemy and item set to load,
    randomly, but not weighted

--- Assets/Scripts/Magic Items/DifficultyTranslator.cs
+++ Assets/Scripts/Magic Items/DifficultyTranslator.cs
@@ -10,13 +10,13 @@ public class DifficultyTranslator : MonoBehaviour
     //public List<Pawn> mediumEnemySet;
     //public List<Pawn> hardEnemySet;
 
-    public EnemySetSO easyEnemySet;
-    public EnemySetSO mediumEnemySet;
-    public EnemySetSO hardEnemySet;
+    public List<EnemySetSO> easyEnemySets; //also SOs
+    public List<EnemySetSO> mediumEnemySets; //also SOs
+    public List<EnemySetSO> hardEnemySets; //also SOs
 
-    public ItemSetSO easyItemSetSO;
-    public ItemSetSO mediumItemSetSO;
-    public ItemSetSO hardItemSetSO;
+    public List<ItemSetSO> easyItemSetSOs;
+    public List<ItemSetSO> mediumItemSetSOs;
+    public List<ItemSetSO> hardItemSetSOs;
 
     public int[] goldRewards; //0,1,2 index - easy, medium, hard
 
@@ -34,19 +34,22 @@ public class DifficultyTranslator : MonoBehaviour
     }
     public List<Pawn> DifficultyToEnemyPreset(LairDifficulty difficulty)
     {
+        int rand;
         //return presets[(int)difficulty];
         switch (difficulty)
         {
             case LairDifficulty.Easy:
-                return easyEnemySet.enemySet.enemyPrefabs;
+                rand = Random.Range(0, easyEnemySets.Count);
+                return easyEnemySets[rand].enemySet.enemyPrefabs;
                 //break;
             case LairDifficulty.Medium:
-                return mediumEnemySet.enemySet.enemyPrefabs; 
-
-                //break;
+                rand = Random.Range(0, mediumEnemySets.Count);
+                return mediumEnemySets[rand].enemySet.enemyPrefabs;
+            //break;
             case LairDifficulty.Hard:
-                return hardEnemySet.enemySet.enemyPrefabs;
-                //break;
+                rand = Random.Range(0, hardEnemySets.Count);
+                return hardEnemySets[rand].enemySet.enemyPrefabs;
+            //break;
             default:
                 return null;
                 //break;
@@ -56,17 +59,25 @@ public class DifficultyTranslator : MonoBehaviour
     
     public MagicItem DifficultyToSingleReward(LairDifficulty difficulty)
     {
+        int rand;
+
         switch (difficulty)
         {
             case LairDifficulty.Easy:
                 //return easyRewardSet[Random.Range(0, easyRewardSet.Count)].magicItem;
-                return WeightedRollOnItemSetSO(easyItemSetSO);
+                rand = Random.Range(0, easyItemSetSOs.Count);
+
+                return WeightedRollOnItemSetSO(easyItemSetSOs[rand]);
                 //break;
             case LairDifficulty.Medium:
-                return WeightedRollOnItemSetSO(mediumItemSetSO);
+                rand = Random.Range(0, mediumItemSetSOs.Count);
+
+                return WeightedRollOnItemSetSO(mediumItemSetSOs[rand]);
             //break;
             case LairDifficulty.Hard:
-                return WeightedRollOnItemSetSO(hardItemSetSO);
+                rand = Random.Range(0, hardItemSetSOs.Count);
+
+                return WeightedRollOnItemSetSO(hardItemSetSOs[rand]);
             //break;
             default:
                 return null;
@@ -85,7 +96,7 @@ public class DifficultyTranslator : MonoBehaviour
             weights[i] = total;
         }
 
-        int rand = Random.Range(0, total);
+        int rand = Random.Range(1, total+1);
         Debug.Log($"Random: {rand}/{total}");
         for (int i = 0; i < weights.Length; i++)
         {
@@ -96,7 +107,10 @@ public class DifficultyTranslator : MonoBehaviour
             }
         }
 
-        return null;
+        ////only case if if it rolled the max number!
+        //Debug.Log("Rolled MAX on items! get another item?");
+
+        return itemSetSO.itemSet.itemSOs[itemSetSO.itemSet.itemSOs.Count-1].magicItem;
     }
 
     public int DifficultyToGoldReward(LairDifficulty difficulty)
