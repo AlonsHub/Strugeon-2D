21fb7da8b6040cadb746d7f097e64957a6ef7dd8
Alon <Alonsouter@gmail.com>
2022-04-12 17:22:40 +0300

    Merc selection available!

--- Assets/MercPoolDisplayer.cs
+++ Assets/MercPoolDisplayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MercPoolDisplayer : MonoBehaviour
 {
@@ -12,4 +13,67 @@ public class MercPoolDisplayer : MonoBehaviour
     //and add generic things to their button's actions, if needed
 
     //this can recieve prefabs for the type of displayer to instantiate, if just modifying the button action isn't enough 
+
+    [SerializeField] GameObject prefab;
+
+    [SerializeField] Transform gridParent;
+
+    List<GameObject> displayers = new List<GameObject>();
+
+    [SerializeField] MercGearDisplayer mercGearDisplayer;
+
+    List<MercSheet> relevant;
+    private void OnEnable()
+    {
+        //int diff = displayers.Count - PlayerDataMaster.Instance.currentPlayerData.mercSheets.Count;
+         relevant = PlayerDataMaster.Instance.GetMercSheetsByAssignments(new List<MercAssignment> { MercAssignment.Available, MercAssignment.Room });
+        int diff = displayers.Count - relevant.Count;
+        if (diff<0)
+        {
+            for (int i = 0; i < diff*-1; i++) //is lower than 0
+            {
+                displayers.Add(Instantiate(prefab, gridParent));
+            }
+        }
+        else if(diff>0)
+        {
+            for (int i = displayers.Count-1; i >= displayers.Count-diff; i--)
+            {
+                GameObject temp = displayers[i];
+                displayers.RemoveAt(i);
+                Destroy(temp);
+            }
+        }
+        //check that they are equal now!
+        diff = displayers.Count - relevant.Count;
+
+        if(diff !=0)
+        {
+            Debug.LogError($"diff ={diff}");
+        }
+
+        for (int i = 0; i < displayers.Count; i++)
+        {
+            EquipMercRosterSlot slot = displayers[i].GetComponent<BasicDisplayer>() as EquipMercRosterSlot;
+            slot.SetMe(new List<string> { }, new List<Sprite> { relevant[i].MyPawnPrefabRef<Pawn>().PortraitSprite });
+
+            slot.index = i;
+            slot.mercPoolDisplayer = this;
+            //displayers[i].GetComponent<Button>().onClick.AddListener(() => OpenGearDisplayerByMercIndex(i));
+
+        }
+       
+    }
+
+    public void OpenGearDisplayerByMercIndex(int i)
+    {
+        //mercGearDisplayer.SetMeFully(PlayerDataMaster.Instance.GetMercSheetByIndex(i));
+        mercGearDisplayer.SetMeFully(relevant[i]);
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
 }
---
+++ Assets/Scripts/UI Scripts/EquipMercRosterSlot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipMercRosterSlot : BasicDisplayer
+{
+    public int index;
+    public MercPoolDisplayer mercPoolDisplayer;
+   
+
+    public void MyOnClick()
+    {
+        mercPoolDisplayer.OpenGearDisplayerByMercIndex(index);
+    }
+}
