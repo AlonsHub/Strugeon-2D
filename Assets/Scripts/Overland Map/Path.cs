7e917a60209f2bb8e10cd5b753d8cd6ea9f73c36
Alon <Alonsouter@gmail.com>
2022-05-31 16:39:41 +0300

    Waypoint and Path

---
+++ Assets/Scripts/Overland Map/Path.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public List<Waypoint> waypoints;
+    List<Vector3> verts;
+
+    public float CalculateLinearDistance()
+    {
+        float distance = 0f;
+
+        // Transalte from transform-data of Waypoints - to vector3 points on the canvas
+        //here is where different types of interpolation are applicable
+        SetVertsLinearPath(1);
+
+        if (verts != null && verts.Count >= 2) //origin and destination are Always waypoints, so minimum count 2
+        {
+            for (int i = 0; i < verts.Count - 1; i++)
+            {
+                distance += Vector3.Distance(verts[i], verts[i + 1]);
+            }
+        }
+        return distance;
+    }
+
+    private void SetVertsLinearPath(int resolution)
+    {
+        if(verts == null)
+        {
+            verts= new List<Vector3>();
+        }
+        for (int i = 0; i < waypoints.Count-1; i++)
+        {
+            //int vertsByDistance = Vector3.Distance
+            for (int k = 0; k < resolution; k++)
+            {
+                //verts.Add(Vector3.Lerp(waypoints[i], waypoints[i+1], ));
+            }
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/Overland Map/Waypoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour //interface instead? TBF - dont like it. no good classes to attach it to
+{
+    Path myPath;
+    List<Waypoint> waypoints { get => myPath.waypoints; set => myPath.waypoints = value; } //this should get the points from the associated path
+
+    public Vector3 position { get => transform.position; set => transform.position = value; }
+}
