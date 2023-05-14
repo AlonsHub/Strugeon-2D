686c8fc45664578bf4c062a336be8cff119e4d1a
Alon <Alonsouter@gmail.com>
2023-05-14 18:09:39 +0300

    Hover location fixed

--- Assets/SacntumItemHover.cs
+++ Assets/SacntumItemHover.cs
@@ -25,7 +25,7 @@ public class SacntumItemHover : HoverableWithBox
     {
         //Debug.LogError($"Rect x: {rectTrans.rect.position.x} y:{rectTrans.rect.position.y}");
         Debug.LogError($"Transform x: {transform.position.x} y:{transform.position.y}");
-        if (transform.position.x> 1200f)
+        if (transform.position.x > 1200f)
         {
             basicDisplayer.transform.localPosition = new Vector3(leftX, basicDisplayer.transform.localPosition.y, basicDisplayer.transform.localPosition.z);
         }
@@ -34,14 +34,10 @@ public class SacntumItemHover : HoverableWithBox
             basicDisplayer.transform.localPosition = new Vector3(rightX, basicDisplayer.transform.localPosition.y, basicDisplayer.transform.localPosition.z);
         }
 
-        if (transform.position.y < 400f)
-        {
-            basicDisplayer.transform.localPosition = new Vector3(basicDisplayer.transform.localPosition.x, topY, basicDisplayer.transform.localPosition.z);
-        }
-        else
-        {
-            basicDisplayer.transform.localPosition = new Vector3(basicDisplayer.transform.localPosition.x, bottomY, basicDisplayer.transform.localPosition.z);
-        }
+        
+
+        basicDisplayer.transform.localPosition = new Vector3(basicDisplayer.transform.localPosition.x, topY - transform.position.y/170, basicDisplayer.transform.localPosition.z);
+
     }
 
     public override void OnPointerEnter(PointerEventData eventData)
