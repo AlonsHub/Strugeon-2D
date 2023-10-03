274cd9586a8a2b668d9e5287a9795f42d145747c
Alon <Alonsouter@gmail.com>
2023-10-03 22:48:01 +0300

    SiteDisplayers done

--- Assets/Scripts/UI Scripts/DwellerDisplayer.cs
+++ Assets/Scripts/UI Scripts/DwellerDisplayer.cs
@@ -13,12 +13,19 @@ public class DwellerDisplayer : MonoBehaviour
     Sprite hiddenSprite;
     [SerializeField]
     Image questionMark;
+    [SerializeField]
+    TMPro.TMP_Text nameText;
+    [SerializeField]
+    GameObject levelBgBox;
+
     public void SetMe(Pawn p, int levelText)
     {
         questionMark.gameObject.SetActive(false);
         portrait.gameObject.SetActive(true);
+        levelBgBox.SetActive(true);
         portrait.sprite = p.FullPortraitSprite;
         level.text = levelText.ToString();
+        nameText.text = p.Name;
     }    
     public void SetMe(Pawn p)
     {
@@ -26,12 +33,16 @@ public class DwellerDisplayer : MonoBehaviour
         portrait.gameObject.SetActive(true);
         portrait.sprite = p.FullPortraitSprite;
         level.text = "";
+        levelBgBox.SetActive(false);
+        nameText.text = p.Name;
     }
     public void SetMe()
     {
         questionMark.gameObject.SetActive(true);
         portrait.gameObject.SetActive(false);
+        levelBgBox.SetActive(false);
         level.text = "";
+        nameText.text = "";
     }
 
     public void KillMe()
